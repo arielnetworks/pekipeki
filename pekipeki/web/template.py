999256b31079a517050faed43c18fce03152a7cc
shoma <shoma.h4a@gmail.com>
2015-02-26 18:35:02 +0900

    add autoescape

--- pekipeki/web/template.py
+++ pekipeki/web/template.py
@@ -6,7 +6,7 @@ import jinja2
 def render(template, **params):
 
     loader = jinja2.PackageLoader('pekipeki.web', 'templates')
-    env = jinja2.Environment(loader=loader)
+    env = jinja2.Environment(loader=loader, autoescape=True)
 
     tmpl = env.get_template(template)
 
